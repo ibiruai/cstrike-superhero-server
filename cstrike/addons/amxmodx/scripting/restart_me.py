87f3cae34a9b6a0bb66fa26307d6e771f02fbd29
evileye <48332831+ibiruai@users.noreply.github.com>
2020-04-05 18:21:51 +0300

    I rewrote the script that restarts HLDS if necessary.

--- cstrike/addons/amxmodx/scripting/restart_me.py
+++
@@ -1,9 +0,0 @@
-import os, time
-
-path = "/home/steam/Half-Life/cstrike/addons/amxmodx/logs/restart_me.log"
-
-if time.time() - os.path.getmtime(path) > 3 * 60:
-	time.sleep(20)
-	if time.time() - os.path.getmtime(path) > 3 * 60:
-		print("\n" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " Server is offline. Restarting.")
-		os.system('sudo shutdown -r 0')
